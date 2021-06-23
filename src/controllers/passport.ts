a3d9fedacc2af63be60c363c21272587b98138db
oluSammy <olumorinsammy@gmail.com>
2021-06-23 22:54:27 +0100

    kk

--- src/controllers/passport.ts
+++ src/controllers/passport.ts
@@ -102,9 +102,9 @@ export const googleAuthController = async (req: Request, res: Response) => {
       token,
       user,
     };
-
-    responseStatus.setSuccess(201, "successful", data);
-    return responseStatus.send(res);
+    return res.redirect(`${process.env.REDIRECT_URL}/${token}`);
+    // responseStatus.setSuccess(201, "successful", data);
+    // return responseStatus.send(res);
   } catch (e) {
     responseStatus.setError(500, "an error occurred");
     return responseStatus.send(res);
